fd100b556c4c19b9b404ca414896693c6ea084cb
Harshita Chaurasia <harshuchauraisa@gmail.com>
2022-07-14 01:06:12 +0530

    added validations on email and username already existing

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,9 @@
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
+from jsonschema import ValidationError
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Length, Email,EqualTo
+from wtforms.validators import DataRequired,Length, Email,EqualTo,ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,6 +12,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])   #password needs to validate
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is taken. Please choose another one')
+
+    def validate_email(self,email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('Email is taken. Please choose another one')
+    
+    
 
 
 class LoginForm(FlaskForm):
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flaskblog import db
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20),unique=True,nullable=False)
     email = db.Column(db.String(120),unique=True,nullable=False)
